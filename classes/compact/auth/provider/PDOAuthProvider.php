0691598e40a81a8a7a593fb522cc46740094fc95
elger <elgervb@gmail.com>
2015-01-17 20:45:22 +0100

    abstract PDO auth provider
    We can connect a database now...
    
    #1

---
+++ classes/compact/auth/provider/PDOAuthProvider.php
@@ -0,0 +1,53 @@
+<?php
+namespace compact\auth\provider;
+
+use compact\repository\pdo\AbstractPDORepository;
+use compact\auth\user\UserModel;
+use compact\auth\ILoginProvider;
+
+/**
+ * Adapter around AbstractPDORepository to provide login functionality
+ *
+ * @author elger
+ */
+class PDOAuthProvider implements ILoginProvider
+{
+
+    private $db;
+
+    public function __construct(AbstractPDORepository $aDb)
+    {
+        $this->db = $aDb;
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see extensions\auth.IAuthProvider::login()
+     */
+    public function login($aUsername, $aPassword)
+    {
+        $sc = $this->db->createSearchCriteria();
+        /* @var $sc \compact\repository */
+        $sc->where(UserModel::USERNAME, $aUsername);
+        $sc->where(UserModel::PASSWORD, $aPassword);
+        
+        $result = $this->db->search($sc);
+        if ($result instanceof \ArrayObject && $result->count() > 0) {
+            /* @var $result \ArrayObject */
+            return $result->offsetGet(0);
+        }
+        
+        return false;
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see \compact\auth\ILoginProvider::getUser()
+     */
+    public function getUser($id)
+    {
+        return $this->db->read($id);
+    }
+}
