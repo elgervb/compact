d991c64e73921506b96fc60a54766aa6070a2a62
elger <elgervb@gmail.com>
2015-01-17 11:51:15 +0100

    more user fields
    #1

--- classes/compact/auth/user/UserModel.php
+++ classes/compact/auth/user/UserModel.php
@@ -1,30 +1,69 @@
 <?php
 namespace compact\auth\user;
 
+/**
+ * Model for the user
+ *
+ * @author elger
+ */
 class UserModel
 {
+
     /**
      * The (generated) id of the user
      */
     const ID = "id";
+
     /**
      * The unique GUID of the user
      */
     const GUID = "guid";
+
     /**
      * The username
      */
     const USERNAME = "username";
+
     /**
      * The encrypted password of the user
      */
     const PASSWORD = "password";
+
     /**
-     * Some room for additional context. This could be anything from a serialized object to an empty value
+     * Some room for additional context.
+     * This could be anything from a serialized object to an empty value
      */
     const CONTEXT = "context";
+
     /**
      * Denotes if the user is active or not
      */
-    const IS_ACTIVE = "isactive";
+    const ACTIVE = "active";
+    
+    /**
+     * The IP address from which the user was registered
+     * @var unknown
+     */
+    CONST IP = "ip";
+    
+    /**
+     * The full name of the user
+     * @var unknown
+     */
+    const FULL_NAME = "full_name";
+    
+    /**
+     * The email address of the user
+     */
+    const EMAIL = "email";
+    
+    /**
+     * The timestamp of when the user was added
+     */
+    const TIMESTAMP = "timestamp";
+    
+    /**
+     * Is the user an admin?
+     */
+    const ADMIN = "admin";
 }
