81500a43723b6b31f64f9d2971f483ee06981cda
elger <elgervb@gmail.com>
2015-02-02 22:37:14 +0100

    comments

--- classes/compact/handler/impl/json/Json.php
+++ classes/compact/handler/impl/json/Json.php
@@ -5,13 +5,25 @@ namespace compact\handler\impl\json;
  */
 class Json
 {
+    /**
+     * The object to serialize to json
+     * @var mixed
+     */
     private $object;
     
+    /**
+     * Creates a new Json object from the $aObject
+     * @param mixed $aObject The object to serialize to json. Normally this would be an array or any object.
+     */
     public function __construct( $aObject )
     {
         $this->object = $aObject;
     }
     
+    /**
+     * Returns the json object
+     * @return mixed
+     */
     public function getObject()
     {
         return $this->object;
