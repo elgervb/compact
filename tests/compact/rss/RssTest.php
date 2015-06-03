1e27d32e3202a3613087ebd5bccad24bbdf7a8f2
elger <elgervb@gmail.com>
2015-06-03 23:44:22 +0200

    add test with Rss Items in it

--- tests/compact/rss/RssTest.php
+++ tests/compact/rss/RssTest.php
@@ -14,4 +14,26 @@ class RssTest extends \PHPUnit_Framework_TestCase
         $xml = $rss->toXml();
         $this->assertEquals( file_get_contents(__DIR__.'/RssTest-testRssChannelXML.xml'), $xml );
     }
+    
+    public function testRssChannelWithItemsXML(){
+        $rss = new Rss();
+        $channel = $rss->getChannel();
+    
+        $channel->set(RssChannel::DESCRIPTION, "Rss Description");
+        $channel->set(RssChannel::LINK, 'http://google.com');
+        $channel->Set(RssChannel::TITLE, "Rss Title");
+        
+        $item = $channel->newItem();
+        $item->set(RssItem::AUTHOR, 'Elger van Boxtel');
+        $item->set(RssItem::DESCRIPTION, "Homepage Elger van Boxtel");
+        $item->set(RssItem::LINK, 'http://www.elgervanboxtel.nl');
+        
+        $item = $channel->newItem();
+        $item->set(RssItem::AUTHOR, 'Elger van Boxtel');
+        $item->set(RssItem::DESCRIPTION, "Startpage Elger van Boxtel");
+        $item->set(RssItem::LINK, 'http://www.elgervanboxtel.nl/labs/start');
+            
+        $xml = $rss->toXml();
+        $this->assertEquals( file_get_contents(__DIR__.'/RssTest-testRssChannelWithItemsXML.xml'), $xml );
+    }
 }
